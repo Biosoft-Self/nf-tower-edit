dd7b1b3f38ef9d2369cf85fdcbfa31d99c479cde
WangYang <1749748955@qq.com>
2023-02-25 13:34:32 +0800

    update

--- tower-web/src/environments/environment.prod.ts
+++ tower-web/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "/api",
+  // apiUrl: "http://10.110.1.11:8088",
+  apiUrl: "http://localhost:8088",
   version: "19.08.0",
   commitId: "63a81b5.dirty",
   captchaKey: null,
